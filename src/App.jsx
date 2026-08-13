ff5f08d17616d7db5b8b45e4bd29ed955ddb430e
Matthew McIntosh <matthew.n.mcintosh@outlook.com>
2026-08-13 11:03:40 -0400

    Added basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import DirectorContainer from "./pages/DirectorContainer"
+
 const App = () => {
     return (
         <>
-        {/* define all routes here */}
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/directors" element={<DirectorContainer />} />
+            </Routes>
+        </BrowserRouter>
         </>
     )
 }
